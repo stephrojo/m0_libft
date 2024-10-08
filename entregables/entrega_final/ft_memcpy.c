bb993270b19aaf652117000961a6f444ddd303cd
Steph Rojo <strojo-h@student.42madrid.com>
2024-10-08 22:50:41 +0200

    memcpy y memmove arreglados, todo ok

--- entregables/entrega_final/ft_memcpy.c
+++ entregables/entrega_final/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: strojo-h <strojo-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:38:23 by strojo-h          #+#    #+#             */
-/*   Updated: 2024/10/01 19:28:40 by strojo-h         ###   ########.fr       */
+/*   Updated: 2024/10/08 22:41:16 by strojo-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*cast_src;
 
 	cast_dest = (unsigned char *)dest;
-	cast_src = (const unsigned char *)src;
+	cast_src = (unsigned char *)src;
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	while (n != 0)
 	{
 		*cast_dest = *cast_src;
--- entregables/entrega_final/ft_memmove.c
+++ entregables/entrega_final/ft_memmove.c
@@ -6,60 +6,34 @@
 /*   By: strojo-h <strojo-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 16:13:38 by strojo-h          #+#    #+#             */
-/*   Updated: 2024/10/01 19:45:23 by strojo-h         ###   ########.fr       */
+/*   Updated: 2024/10/08 22:45:25 by strojo-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h> // size_t
-#include <stdlib.h> // malloc y free
-
-static void	src_to_temp(const unsigned char *cast_src, size_t n, unsigned char
-*temp_buffer)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		temp_buffer[i] = cast_src[i];
-		i++;
-	}
-}
-
-static void	temp_to_dest(unsigned char *cast_dest, size_t n, unsigned char
-*temp_buffer)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		cast_dest[i] = temp_buffer[i];
-		i++;
-	}
-}
+#include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char		*temp_buffer;
 	unsigned char		*cast_dest;
 	const unsigned char	*cast_src;
 
 	cast_dest = (unsigned char *)dest;
 	cast_src = (const unsigned char *)src;
-	temp_buffer = (unsigned char *)malloc(n);
-	if (!temp_buffer)
-		return (NULL);
 	if (dest == src || n == 0)
-	{
-		free(temp_buffer);
 		return (dest);
+	if (cast_src < cast_dest)
+	{
+		while (n--)
+			cast_dest[n] = cast_src[n];
 	}
 	else
 	{
-		src_to_temp(cast_src, n, temp_buffer);
-		temp_to_dest(cast_dest, n, temp_buffer);
+		while (n--)
+		{
+			*cast_dest = *cast_src;
+			cast_dest++;
+			cast_src++;
+		}
 	}
-	free(temp_buffer);
 	return (dest);
 }
