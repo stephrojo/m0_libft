38a651fb57fd763550a43e42139d78cf736b43d0
Steph Rojo <strojo-h@student.42madrid.com>
2024-10-18 00:51:49 +0200

    cambios en substr

--- entregables_obligatorio/ft_substr.c
+++ entregables_obligatorio/ft_substr.c
@@ -6,51 +6,30 @@
 /*   By: strojo-h <strojo-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 23:33:43 by strojo-h          #+#    #+#             */
-/*   Updated: 2024/10/14 17:09:21 by strojo-h         ###   ########.fr       */
+/*   Updated: 2024/10/18 00:05:33 by strojo-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*malloc_substr(size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
+	size_t	size;
 
-	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(&s[start]);
+	if (size < len)
+		len = size;
+	if (len + 1 > len)
+		substr = malloc((len + 1) * sizeof(char));
+	else
+		substr = malloc(len * sizeof(char));
 	if (!substr)
 		return (NULL);
-	return (substr);
-}
-
-static size_t	substr_len(const char *s, unsigned int start, size_t len)
-{
-	size_t	actual_len;
-
-	actual_len = ft_strlen(s);
-	if (start >= actual_len)
-		return (0);
-	if (actual_len - start < len)
-		len = actual_len - start;
-	return (len);
-}
-
-char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	char	*substr;
-	size_t	i;
-
-	if (!s)
-		return (NULL);
-	len = substr_len(s, start, len);
-	substr = malloc_substr(len);
-	if (!substr)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = '\0';
+	ft_strlcpy(substr, &s[start], len + 1);
 	return (substr);
 }
